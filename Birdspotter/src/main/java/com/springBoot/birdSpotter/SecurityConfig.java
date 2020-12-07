1fae24ca776a3d7017aeadbe6c7f51ceed21236e
Jonas Haenebalcke <jonashaenebalcke@hotmail.be>
2020-12-07 16:05:24 +0100

    Add Login Start
    Login works, but is not necessary to continue (this is a bug but I'm making a feature out of it so I don't need to keep logging in to test stuff)

---
+++ Birdspotter/src/main/java/com/springBoot/birdSpotter/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.springBoot.birdSpotter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+		auth.inMemoryAuthentication().withUser("spotter").password("{noop}RedDuck007").roles("SPOTTER").and()
+				.withUser("admin").password("{noop}eagle").roles("SPOTTER", "ADMIN");
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+//		http.formLogin().defaultSuccessUrl("/birdspotting", true).loginPage("/login");
+//
+//		http.authorizeRequests().antMatchers("/birdspotting").hasRole("SPOTTER")
+//		.and().csrf();
+//
+		http.formLogin().defaultSuccessUrl("/birdspotting", true).loginPage("/login").usernameParameter("username")
+				.passwordParameter("password").and().exceptionHandling().accessDeniedPage("/403").and().csrf();
+
+//		http.formLogin().defaultSuccessUrl("/birdspotting", true).loginPage("/login");
+		
+	}
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//		.antMatchers("/403*").permitAll()
+//		.antMatchers("/*").hasRole("SPOTTER");
+//		
+//		http.formLogin().defaultSuccessUrl(")
+//	}
+
+}
---
+++ Birdspotter/src/main/webapp/views/login.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body onload='document.loginForm.username.focus();'>
+	<h1>Bird Spotting Login</h1>
+
+	<h3>Login with Username and Password</h3>
+
+	<c:if test="${not empty error}">
+		<div class="error">${error}</div>
+	</c:if>
+	
+	<c:if test="${not empty msg}">
+		<div class="msg">${msg}</div>
+	</c:if>
+	
+
+	<form action='login' method='POST'>
+
+		<table>
+			<tr>
+				<td>User</td>
+				<td><input type="text" name="username" value=""></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password" value=""></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input name="submit" type="submit"
+					value="submit"></td>
+			</tr>
+		</table>
+
+		<input type="hidden" name="${_csrf.parameterName}"
+			value="${_csrf.token}" /><
+	</form>
+</body>
+</html>
