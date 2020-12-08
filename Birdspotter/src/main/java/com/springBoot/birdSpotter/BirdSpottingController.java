bf0e4b0008ba7e798ac9691f2328a6d3bb16e343
Jonas Haenebalcke <jonashaenebalcke@hotmail.be>
2020-12-08 15:23:07 +0100

    Add LocationDetails

--- Birdspotter/src/main/java/com/springBoot/birdSpotter/BirdSpottingController.java
+++ Birdspotter/src/main/java/com/springBoot/birdSpotter/BirdSpottingController.java
@@ -1,24 +1,49 @@
 package com.springBoot.birdSpotter;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import domain.BirdSpotLocation;
+import domain.SpottedBird;
 import service.SpottedBirdService;
 
 @Controller
-@RequestMapping("/birdspotting")	
+@RequestMapping("/birdspotting")
 public class BirdSpottingController {
-	
+
 	@Autowired
 	private SpottedBirdService spottedBirdService;
-	
+
 	@GetMapping(value = "")
 	public String ListLocations(Model model) {
 		model.addAttribute("locationList", spottedBirdService.findAll());
 		return "locationList";
+	}
+
+	@GetMapping(value = "/{name}")
+	public String spottedBirds(@PathVariable("name") String locationName, Model model) {
+
+		Optional<BirdSpotLocation> location = spottedBirdService.findByName(locationName);
+
+		if (location == null)
+			return "locationList";
+
+		List<SpottedBird> spottedBirds = location.get().getSpottedBirds();
+		model.addAttribute("location", location);
+		model.addAttribute("spottedBirds", spottedBirds);
+		return "locationDetails";
+	}
+
+	@GetMapping(value = "/{location}/newbirdspotting")
+	public String AddSpotting(@PathVariable("location") String location, Model model) {
+		return "addSpotting";
 	}
 
 }
--- Birdspotter/src/main/webapp/views/locationDetails.jsp
+++ Birdspotter/src/main/webapp/views/locationDetails.jsp
@@ -1,12 +1,43 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
+<link href="../resources/css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-SPECIFIEKE PAGINA
+	<h1>Overview of spotted birds in ${location.get().getName()}</h1>
+
+	<br>
+	<p>
+		<a href="/birdspotting/${location.get().getName()}/newbirdspotting"}>New
+			Spotting</a> - <a href="/birdspotting">Spotting locations overview</a>
+	</p>
+	<br>
+
+	<table>
+		<tr>
+			<th>Index</th>
+			<th>Specie</th>
+			<th>Year of discovery</th>
+			<th>Code index in the book of birds</th>
+			<th>Spottings</th>
+		</tr>
+		<c:forEach items="${spottedBirds}" var="spottedBirds"
+			varStatus="status">
+			<tr>
+
+				<td>${status.index +1}</td>
+				<td>${spottedBirds.birdSpecie.name}</td>
+				<td>${spottedBirds.birdSpecie.yearOfDiscovery}</td>
+				<td>${spottedBirds.birdSpecie.code}</td>
+				<td>${spottedBirds.count}</td>
+			</tr>
+
+		</c:forEach>
+		<table>
 </body>
 </html>
